dd29f745c2ebb3fca9036d5aa81994369d000988
Anderson <anderson@prodwin.com.br>
2021-10-15 17:36:30 -0300

    Implementado a controller de Fornecedores
    Implementado a controller de Fornecedores

---
+++ MinhaAPICompleta/src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // Validacao de notificações e erro
+
+        // validação de ModelState
+
+        // Validação da Operação
+    }
+}
--- MinhaAPICompleta/src/DevIO.Api/Startup.cs
+++ MinhaAPICompleta/src/DevIO.Api/Startup.cs
@@ -1,7 +1,10 @@
+using DevIO.Api.Configurations;
+using DevIO.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,13 +25,20 @@ namespace DevIO.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MeuDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddControllers();
+
+            services.ResolveDependencies();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
